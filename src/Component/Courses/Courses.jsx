a9279e2f882352307886d8b2f0b0f84837949308
Aryan Dev <nobodygivesafak@gmail.com>
2022-09-28 18:35:22 +0530

    Update Courses.jsx

--- src/Component/Courses/Courses.jsx
+++ src/Component/Courses/Courses.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Courses = () => {
-  return <div>Courses</div>;
+  return (
+    <div>
+      Courses
+      <div>WTf WTf WTf WTf WTf</div>
+    </div>
+  );
 };
 
 export default Courses;
