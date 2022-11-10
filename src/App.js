0eec05fd959d44b4b264bc4190e2077eee3c00fc
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2022-11-10 20:51:10 +0530

    about now

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Signup } from "./Components/Signup";
-import { Login } from "./Components/Login";
+import { Signup } from "./Components/signup";
+import { Login } from "./Components/login";
+import { CalculateBMI } from "./Components/calculateBMI";
+import { Logout } from "./Components/logout";
+import { useEffect, useState } from "react";
+import { Navbar } from "./Components/navbar";
 
 function App() {
+  const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("token") === null) {
+      setAuth(false);
+    } else {
+      setAuth(true);
+    }
+  }, [setAuth]);
   return (
     <div className="App">
       <header className="App-header">
+        {auth ? <Logout /> : <Navbar />}
+        <br />
         <Routes>
-          <Route path="/" element={<Signup />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/" element={<CalculateBMI />} />
         </Routes>
       </header>
     </div>
   );
 }
 
-export default App;
+export { App };
